85436041856265ba022e81d68f78a1f18d157201
unknown <muktinathrajbanshi786@gmail.com>
2026-01-09 10:35:48 +0545

    wow logic started ho gaya re baba

---
+++ script.js
@@ -0,0 +1,6 @@
+const select = document.querySelector("select");
+const inputAll = document.querySelector("input");
+const myImg = document.querySelector("img");
+const textArea = document.querySelector("textarea");
+
+
