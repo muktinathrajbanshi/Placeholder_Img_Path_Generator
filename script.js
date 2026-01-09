719b1e72be2527d632c08d86c3bc815251c0c407
unknown <muktinathrajbanshi786@gmail.com>
2026-01-09 12:40:14 +0545

    wow that's awesome man

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const select = document.querySelector("select");
-const inputAll = document.querySelector("input");
-const myImg = document.querySelector("img");
-const textArea = document.querySelector("textarea");
+const inputAll = document.querySelectorAll("input");
+let myImg = document.querySelector("img");
+let textArea = document.querySelector("textarea");
+
+console.log(inputAll);
 
 
+const  createImagePath = () => {
+
+    let urlPath = `https://placehold.co/${select.value}`;
+
+    myImg.src = urlPath;
+    textArea.value = urlPath;
+}
+
+inputAll.forEach((curElem) => curElem.addEventListener("change", createImagePath));
+
+select.addEventListener("change", createImagePath);
